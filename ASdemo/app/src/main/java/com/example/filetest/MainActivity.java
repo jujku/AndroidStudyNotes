d71fcb86f5c731398548f1e905d0277ded4706a5
jujku <yushan.work@qq.com>
2024-07-09 11:43:52 +0800

    6.2.2 sharePreferences的使用

--- ASdemo/app/src/main/java/com/example/filetest/MainActivity.java
+++ ASdemo/app/src/main/java/com/example/filetest/MainActivity.java
@@ -1,24 +1,39 @@
 package com.example.filetest;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
     private EditText edit;
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edit = (EditText) findViewById(R.id.edit_query);
+
+        String inputText = load();
+        if(!TextUtils.isEmpty(inputText)){
+            edit.setText(inputText);
+            edit.setSelection(inputText.length());
+            Toast.makeText(this,"Restoring succeeded",Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -47,4 +62,30 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+
+    private String load(){
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try{
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while((line = reader.readLine()) != null){
+                content.append(line);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(reader != null){
+                try{
+                    reader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
     }
+    }
+
