c522c30770c1d154c5df129ac06e21dd888b64c7
jujku <yushan.work@qq.com>
2024-07-17 16:40:11 +0800

    xml解析

--- ASDemo/NetworkTest/app/src/main/java/com/example/networktest/MainActivity.java
+++ ASDemo/NetworkTest/app/src/main/java/com/example/networktest/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.networktest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -20,6 +25,8 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
 
     TextView responseText;
 
@@ -46,11 +53,12 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("https://www.baidu.com")
+                            .url("http://192.168.40.23:80/get_data.xml")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
-                    showResponse(responseData);
+//                    showResponse(responseData);
+                    parseXMLWithPull(responseData);
                 }catch (Exception e){
                     e.printStackTrace();
                 }
@@ -58,6 +66,45 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
+    private void parseXMLWithPull(String xmlData){
+        try{
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            String id = "";
+            String name = "";
+            String version = "";
+            while(eventType != XmlPullParser.END_DOCUMENT){
+                String nodeName = xmlPullParser.getName();
+                switch(eventType){
+                    case XmlPullParser.START_TAG:{
+                        if("id".equals(nodeName)){
+                            id = xmlPullParser.nextText();
+                        }else if("name".equals(nodeName)){
+                            name = xmlPullParser.nextText();
+                        }else if("version".equals(nodeName)){
+                            version = xmlPullParser.nextText();
+                        }
+                        break;
+                   }
+                //完成解析某个节点
+                    case XmlPullParser.END_TAG:{
+                    if("app".equals(nodeName)){
+                        Log.d(TAG, "id is "+ id);
+                        Log.d(TAG, "name is "+ name);
+                        Log.d(TAG, "version is "+ version);
+                    }
+                    break;
+                    }
+                    default:
+                        break;
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            }
+    }
     private void sendRequestWithHttpURLConnection(){
         //开启线程来发起网络请求
         new Thread(new Runnable(){
