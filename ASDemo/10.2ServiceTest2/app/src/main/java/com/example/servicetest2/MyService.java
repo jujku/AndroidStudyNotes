d69af6bfec85661b7662ad8010343a7f5d2dde4a
jujku <yushan.work@qq.com>
2024-07-18 13:41:54 +0800

    startForeground

--- ASDemo/10.2ServiceTest2/app/src/main/java/com/example/servicetest2/MyService.java
+++ ASDemo/10.2ServiceTest2/app/src/main/java/com/example/servicetest2/MyService.java
@@ -1,12 +1,23 @@
 package com.example.servicetest2;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.RemoteViews;
+
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
+
+    private static final String CHANNEL_ID = "ForegroundServiceChannel";
 
     private static String TAG = "MyService";
     private DownloadBinder mBinder = new DownloadBinder();
@@ -33,6 +44,27 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "onCreate: ");
+
+        createNotificationChannel();
+
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.notification_layout);
+
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
+        Notification notification = new NotificationCompat.Builder(this,CHANNEL_ID)
+                .setContentText("this is content text")
+                .setContentTitle("this is content title")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                .setStyle(new NotificationCompat.DecoratedCustomViewStyle())
+                .setCustomContentView(remoteViews)
+                .setCustomHeadsUpContentView(remoteViews)
+                .setCustomBigContentView(remoteViews)
+                .setContentIntent(pi)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build();
+        startForeground(1,notification);
     }
 
     @Override
@@ -46,5 +78,19 @@ public class MyService extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: ");
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            if (manager != null) {
+                manager.createNotificationChannel(serviceChannel);
+            }
+        }
     }
 }
