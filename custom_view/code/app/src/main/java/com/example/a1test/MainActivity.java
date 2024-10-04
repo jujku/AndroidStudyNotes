8dada01932aeec0572d19c418d2493813855fbb7
xinyushan <xinyushan@cooseagroup.com>
2024-10-04 17:48:07 +0800

    双色文字

--- custom_view/code/app/src/main/java/com/example/a1test/MainActivity.java
+++ custom_view/code/app/src/main/java/com/example/a1test/MainActivity.java
@@ -6,11 +6,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
 
+import com.example.a1test.view.ColorTrackTextView;
 import com.example.a1test.view.QQStepView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +24,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 //        QQStepView();
+        Button btn = findViewById(R.id.start);
+        Button btn2 = findViewById(R.id.end_start);
+        btn.setOnClickListener(v -> {
+            Log.d(TAG, "onCreate: " + "点击");
+            colorTraceTextViwe();
+        });
+        btn2.setOnClickListener(v->{
+            colorTraceTextViwe2();
+        });
 
+    }
+    private void colorTraceTextViwe() {
+        ColorTrackTextView colorTrackTextView = findViewById(R.id.color_track_text_view);
+        colorTrackTextView.setDirection(ColorTrackTextView.Direction.LEFT_TO_RIGHT);
+        ValueAnimator valueAnimator = ObjectAnimator.ofFloat(0, 1);
+        valueAnimator.setDuration(2000);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(@NonNull ValueAnimator animation) {
+                float currentProgress = (float) animation.getAnimatedValue();
+                colorTrackTextView.setCurrentProgress(currentProgress);
+            }
+        });
+    valueAnimator.start();
+    }
+    private void colorTraceTextViwe2() {
+        ColorTrackTextView colorTrackTextView = findViewById(R.id.color_track_text_view);
+        colorTrackTextView.setDirection(ColorTrackTextView.Direction.RIGHT_TO_LEFT);
+        ValueAnimator valueAnimator = ObjectAnimator.ofFloat(0, 1);
+        valueAnimator.setDuration(2000);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(@NonNull ValueAnimator animation) {
+                float currentProgress = (float) animation.getAnimatedValue();
+                colorTrackTextView.setCurrentProgress(currentProgress);
+            }
+        });
+        valueAnimator.start();
     }
 
     private int currentSteps;
--- custom_view/code/app/src/main/java/com/example/a1test/view/ColorTrackTextView.java
+++ custom_view/code/app/src/main/java/com/example/a1test/view/ColorTrackTextView.java
@@ -1,13 +1,29 @@
 package com.example.a1test.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
-public class ColorTrackTextView extends TextView {
+import com.example.a1test.R;
 
+public class ColorTrackTextView extends TextView {
+    private Paint mOriginPaint;
+    private Paint mChagnePaint;
+    private float mCurrentProgress = 0.0f ;
+
+    private Direction mDirection;
+    private String TAG;
+
+    public enum Direction{
+        LEFT_TO_RIGHT,RIGHT_TO_LEFT
+    }
 
     public ColorTrackTextView(Context context) {
         this(context,null);
@@ -19,5 +35,74 @@ public class ColorTrackTextView extends TextView {
 
     public ColorTrackTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initPaint(context,attrs);
+    }
+
+    private void initPaint(Context context,AttributeSet attrs){
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.ColorTrackTextView);
+
+        int originColor = array.getColor(R.styleable.ColorTrackTextView_originColor,getTextColors().getDefaultColor());
+        int changeColor = array.getColor(R.styleable.ColorTrackTextView_changeColor,getTextColors().getDefaultColor());
+
+         mOriginPaint = getPaintByColor(originColor);
+         mChagnePaint = getPaintByColor(changeColor);
+
+         array.recycle();
+    }
+
+    private Paint getPaintByColor(int color){
+        Paint paint = new Paint();
+        paint.setColor(color);
+        paint.setAntiAlias(true);
+        paint.setDither(true);
+        paint.setTextSize(getTextSize());
+        return paint;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+
+        int middle = (int) (mCurrentProgress * getWidth());
+
+        if(mDirection == Direction.LEFT_TO_RIGHT){
+            drawText(canvas,mChagnePaint,0,middle);
+            //绘制变色
+            drawText(canvas,mOriginPaint,middle,getWidth());
+        }else if(mDirection == Direction.RIGHT_TO_LEFT){
+            drawText(canvas,mChagnePaint,middle,getWidth());
+            //绘制变色
+            drawText(canvas,mOriginPaint,0,middle);
+        }else{
+            drawText(canvas,mOriginPaint,middle,getWidth());
+        }
+    }
+
+    private void drawText(Canvas canvas,Paint paint,int start,int end){
+        canvas.save();
+        //不变色的地方
+        Rect rect = new Rect(start,0,end,getHeight());
+        canvas.clipRect(rect);
+        //我们自己来画
+        String text = getText().toString();
+        Rect bounds = new Rect();
+
+        paint.getTextBounds(text,0,text.length(),bounds);
+        int x = getWidth() / 2 - bounds.width() / 2;
+        Paint.FontMetricsInt fontMetrics = paint.getFontMetricsInt();
+        int dy = (fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom;
+        int baseLine = getHeight() /2 + dy;
+        canvas.drawText(text,x,baseLine,paint);
+        canvas.restore();
+    }
+
+    public void setDirection(Direction direction){
+        this.mDirection = direction;
+    }
+
+    public void setCurrentProgress(float currentProgress){
+        Log.d(TAG, "setCurrentProgress: " + "设置");
+        this.mCurrentProgress = currentProgress;
+        invalidate();
     }
 }
